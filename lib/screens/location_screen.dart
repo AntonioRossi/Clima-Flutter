01e6152d0358b92e9d8baff762c7c1c0dea2852f
Antonio Rossi <antoniorossi@outlook.com>
2020-07-21 17:43:49 +0200

    update city_screen

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
-
-import '../services/weather.dart';
-import '../services/weather.dart';
+import 'package:clima/screens//city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   final locationData;
@@ -75,7 +74,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
